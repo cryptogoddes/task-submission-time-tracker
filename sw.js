7f16325ba9c31d0342f5b857797e178172f7bc35
cryptogoddes <camlywills@gmail.com>
2026-05-26 18:19:05 -0400

    initial commit

---
+++ sw.js
@@ -0,0 +1,43 @@
+
+const CACHE_NAME = 'cambranth-timer-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install Event: Save assets to browser cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event: Cleanup older caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event: Serve cached versions immediately if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
