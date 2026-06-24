ded4badcf85f626129e70cf1ef00bba5ed369100
Nwamadu Confidence Chidinma <109877291+cryptogoddes@users.noreply.github.com>
2026-06-24 01:48:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,41 @@
-
-const CACHE_NAME = 'cambranth-timer-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  'https://cdn.tailwindcss.com'
-];
+const CACHE_NAME = "cambranth-timer-v2";
+const ASSETS_TO_CACHE = ["./", "./index.html", "https://cdn.tailwindcss.com"];
 
 // Install Event: Save assets to browser cache
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
         return cache.addAll(ASSETS_TO_CACHE);
       })
-      .then(() => self.skipWaiting())
+      .then(() => self.skipWaiting()),
   );
 });
 
 // Activate Event: Cleanup older caches
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys.map((key) => {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+          }),
+        );
+      })
+      .then(() => self.clients.claim()),
   );
 });
 
 // Fetch Event: Serve cached versions immediately if offline
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        return cachedResponse || fetch(event.request);
-      })
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    }),
   );
 });
